44707a8853ec7d3cf909f43b693b386f21b4995b
drosh <droshankumar55@gmail.com>
2025-10-22 09:30:23 +0530

    second commit

---
+++ lib/answer_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  final String answerText;
+  final Function() onTap;
+  const AnswerButton(this.answerText,this.onTap,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20
+        ),
+        backgroundColor: const Color.fromARGB(255, 18, 45, 92),
+        foregroundColor: Colors.white70,
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -8,9 +10,30 @@ class QuestionScreen extends StatefulWidget {
   }
 }
 
-class _QuestionScreenState extends State<QuestionScreen>{
+class _QuestionScreenState extends State<QuestionScreen> {
   @override
-  Widget build(BuildContext context){
-    return Text('hello');
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(
+              color: const Color.fromARGB(255, 222, 218, 218),
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(height: 30),
+          AnswerButton(currentQuestion.answers[0], () {}),
+          AnswerButton(currentQuestion.answers[1], () {}),
+          AnswerButton(currentQuestion.answers[2], () {}),
+          AnswerButton(currentQuestion.answers[3], () {}),
+        ],
+      ),
+    );
   }
 }
